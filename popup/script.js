4b87d7aafa5b1fbc8ccfcb43beff6d48412278dc
Md Saif <saifskd3@gmail.com>
2023-07-26 20:02:21 +0530

    deleted popup

--- popup/script.js
+++
@@ -1,8 +0,0 @@
-let popup = document.getElementById("popup");
-
-function opanpopup() {
-    popup.classList.add("popup-opan");
-}
-function closepopup() {
-    popup.classList.remove("popup-opan");
-}
